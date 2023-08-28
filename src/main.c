348f9034eb5cb6bc9a246a23207452fbe8a59c57
lucasgurgeln <lucasgurgel09@hotmail.com>
2023-08-28 19:37:30 -0300

    feito

--- src/main.c
+++ src/main.c
@@ -4,9 +4,128 @@
 
 #include "asf.h"
 
+/**
+ * \brief Set a high output level on all the PIOs defined in ul_mask.
+ * This has no immediate effects on PIOs that are not output, but the PIO
+ * controller will save the value if they are changed to outputs.
+ *
+ * \param p_pio Pointer to a PIO instance.
+ * \param ul_mask Bitmask of one or more pin(s) to configure.
+ */
+void _pio_set(Pio *p_pio, const uint32_t ul_mask)
+{
+	p_pio->PIO_SODR = ul_mask;
+
+}
+
+
+/**
+ * \brief Set a low output level on all the PIOs defined in ul_mask.
+ * This has no immediate effects on PIOs that are not output, but the PIO
+ * controller will save the value if they are changed to outputs.
+ *
+ * \param p_pio Pointer to a PIO instance.
+ * \param ul_mask Bitmask of one or more pin(s) to configure.
+ */
+void _pio_clear(Pio *p_pio, const uint32_t ul_mask)
+{
+	p_pio->PIO_CODR = ul_mask;
+}
+
+/**
+ * \brief Configure PIO internal pull-up.
+ *
+ * \param p_pio Pointer to a PIO instance.
+ * \param ul_mask Bitmask of one or more pin(s) to configure.
+ * \param ul_pull_up_enable Indicates if the pin(s) internal pull-up shall be
+ * configured.
+ */
+void _pio_pull_up(Pio *p_pio, const uint32_t ul_mask,
+        const uint32_t ul_pull_up_enable){
+					p_pio->PIO_PUER = ul_mask;
+ }
+
+ /**
+ * \brief Configure one or more pin(s) or a PIO controller as inputs.
+ * Optionally, the corresponding internal pull-up(s) and glitch filter(s) can
+ * be enabled.
+ *
+ * \param p_pio Pointer to a PIO instance.
+ * \param ul_mask Bitmask indicating which pin(s) to configure as input(s).
+ * \param ul_attribute PIO attribute(s).
+ */
+void _pio_set_input(Pio *p_pio, const uint32_t ul_mask,
+        const uint32_t ul_attribute)
+{
+	p_pio->PIO_PUER = ul_mask;
+	p_pio->PIO_ODR = ul_mask;
+}
+
+/**
+ * \brief Configure one or more pin(s) of a PIO controller as outputs, with
+ * the given default value. Optionally, the multi-drive feature can be enabled
+ * on the pin(s).
+ *
+ * \param p_pio Pointer to a PIO instance.
+ * \param ul_mask Bitmask indicating which pin(s) to configure.
+ * \param ul_default_level Default level on the pin(s).
+ * \param ul_multidrive_enable Indicates if the pin(s) shall be configured as
+ * open-drain.
+ * \param ul_pull_up_enable Indicates if the pin shall have its pull-up
+ * activated.
+ */
+void _pio_set_output(Pio *p_pio, const uint32_t ul_mask,
+        const uint32_t ul_default_level,
+        const uint32_t ul_multidrive_enable,
+        const uint32_t ul_pull_up_enable)
+{
+	p_pio->PIO_PER = ul_mask;
+	p_pio->PIO_OER = ul_mask;
+	p_pio->PIO_SODR = ul_mask;
+
+}
+
+/**
+ * \brief Return 1 if one or more PIOs of the given Pin instance currently have
+ * a high level; otherwise returns 0. This method returns the actual value that
+ * is being read on the pin. To return the supposed output value of a pin, use
+ * _pio_get_output_data_status() instead.
+ *
+ * \param p_pio Pointer to a PIO instance.
+ * \param ul_type PIO type.
+ * \param ul_mask Bitmask of one or more pin(s) to configure.
+ *
+ * \retval 1 at least one PIO currently has a high level.
+ * \retval 0 all PIOs have a low level.
+ */
+uint32_t _pio_get(Pio *p_pio, const pio_type_t ul_type,
+        const uint32_t ul_mask)
+{
+
+	return (p_pio->PIO_PDSR & ul_mask);
+}
+
+
+uint32_t _delay_ms(uint32_t ms)
+{
+	uint32_t i;
+	for (i = 0; i < ms; i++) {
+		delay_us(1000);
+	}
+	return 0;
+}
 /************************************************************************/
 /* defines                                                              */
 /************************************************************************/
+
+/*  Default pin configuration (no attribute). */
+#define _PIO_DEFAULT             (0u << 0)
+/*  The internal pin pull-up is active. */
+#define _PIO_PULLUP              (1u << 0)
+/*  The internal glitch filter is active. */
+#define _PIO_DEGLITCH            (1u << 1)
+/*  The internal debouncing filter is active. */
+#define _PIO_DEBOUNCE            (1u << 3)
 
 #define LED_PIO PIOC       // periferico que controla o LED
 #define LED_PIO_ID ID_PIOC // ID do periférico PIOC (controla LED)
